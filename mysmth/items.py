d9b65db5f8e949676e74e00f839d75af9bc01466
Guoyi <4345511@qq.com>
2022-05-01 07:29:22 +0800

    增加帖子主题的爬取

--- mysmth/items.py
+++ mysmth/items.py
@@ -9,6 +9,7 @@ import scrapy
 # 需要存储的字段
 class MysmthItem(scrapy.Item):
     # define the fields for your item here like:
+    title = scrapy.Field()
     msgid = scrapy.Field()
     refid = scrapy.Field()
     name = scrapy.Field()
--- mysmth/spiders/smth.py
+++ mysmth/spiders/smth.py
@@ -1,4 +1,5 @@
 import re
+from turtle import title
 from lxml import etree
 import requests
 import scrapy
@@ -9,20 +10,32 @@ from ..items import MysmthItem
 class SmthSpider(scrapy.Spider):
     name = 'smth'
     allowed_domains = ['mysmth.net']
-    start_urls = ['https://www.mysmth.net/nForum/board/MilitaryView?ajax&p=%s' % i for i in range(1, 153)]
+    start_urls = ['https://www.mysmth.net/nForum/board/MilitaryView?ajax&p=%s' % i for i in range(1, 2)]
 
     def parse(self, response, **kwargs):
         items = MysmthItem()
         selector = Selector(response)
         texts = selector.xpath("//td[@class='title_9']/a/@href").extract()
+        
         page_nums = selector.xpath("//tbody/tr/td[contains(@class, 'title_11')][3]/text()").extract()
         for i, j in zip(page_nums, texts):
+
             msg_id = re.search(r'\d+', j)
             msg_id = msg_id.group()
+
             for t in range(1, int(i) // 10 + 2):
                 url = "https://www.mysmth.net" + j + "?ajax&p=%s" % t
+
                 res1 = requests.get(url)
                 html1 = etree.HTML(res1.text)
+                print("1111---------------------1111")      
+                #print ("\n".join(res1))          
+                #print ("\n".join(html1)) 
+                print(res1.text)      
+                print("2222---------------------2222") 
+                title = html1.xpath("/html/head/title/text()")[0]
+                print("3333---------------------3333") 
+                print(title)
                 names = html1.xpath("//div[@class='a-u-uid']")
                 contents = html1.xpath("//td[@class='a-content']")
                 floor = 0
@@ -39,6 +52,7 @@ class SmthSpider(scrapy.Spider):
                         content = ",".join(content.split("\n")).replace("\xa0\xa0", ",")
                         new_content, num = re.subn('<br>', ',', content)
                         if t == 1 and floor == 0:
+                            items["title"] = title
                             items["msgid"] = msg_id
                             items["refid"] = -1
                         else:
